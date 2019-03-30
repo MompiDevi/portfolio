b300223cde4c7b74cc546d5537024daaf02e7a08
MompiDevi <mompi.momiv@gmail.com>
2019-03-30 16:17:51 +0530

    Updates

---
+++ precache-manifest.6f42aea034f3530cd2a32788a1a43fec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "2148ea2f3f0cce9274de",
+    "url": "/portfolio/static/js/main.6a06dbfb.chunk.js"
+  },
+  {
+    "revision": "0571b69cddc311e66ec0",
+    "url": "/portfolio/static/js/2.b934461c.chunk.js"
+  },
+  {
+    "revision": "2148ea2f3f0cce9274de",
+    "url": "/portfolio/static/css/main.51fef30d.chunk.css"
+  },
+  {
+    "revision": "0dc1e2a4a77de9420f24ff36a67c9f59",
+    "url": "/portfolio/index.html"
+  }
+];
